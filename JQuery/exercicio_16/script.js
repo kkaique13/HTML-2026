fb2364aff07e7a8752ad0b151649f8121bad18c0
kkaique <kaique.s.reis2009@gmail.com>
2026-09-21 13:32:30 -0300

    Fiz os exercícios 16 e 17

---
+++ JQuery/exercicio_16/script.js
@@ -0,0 +1,17 @@
+function green(){
+    $(".siga").css("background-color", "green")
+    $(".aten").css("background-color", "black")
+    $(".pare").css("background-color", "black")
+}
+
+function yellow(){
+    $(".siga").css("background-color", "black")
+    $(".aten").css("background-color", "yellow")
+    $(".pare").css("background-color", "black")
+}
+
+function red(){
+    $(".siga").css("background-color", "black")
+    $(".aten").css("background-color", "black")
+    $(".pare").css("background-color", "red")
+}
---
+++ JQuery/exercicio_17/script.js
@@ -0,0 +1,9 @@
+function ligar(){
+    $("#lamp").attr("src", "https://img.freepik.com/vetores-gratis/lampada-realista-com-eletricidade_23-2149129410.jpg?semt=ais_hybrid")
+    $("#lamp").attr("height", "500")
+}
+
+function desligar(){
+    $("#lamp").attr("src", "https://fernandoleonid.github.io/mini-projetos-js/01-lamp/img/desligada.jpg")
+    $("#lamp").attr("height", "500")
+}
