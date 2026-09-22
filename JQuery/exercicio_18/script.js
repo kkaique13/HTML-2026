426330e8d28c5fe39b74bc7815af88b817c5e519
kkaique <kaique.s.reis2009@gmail.com>
2026-09-21 21:52:09 -0300

    Fiz os exercícios 17 e 18

---
+++ JQuery/exercicio_18/script.js
@@ -0,0 +1,9 @@
+function escuro(){
+    $('body').css('background-color', 'black')
+    $('body').css('color', 'white')
+}
+
+function claro(){
+    $('body').css('background-color', 'white')
+    $('body').css('color', 'black')
+}
